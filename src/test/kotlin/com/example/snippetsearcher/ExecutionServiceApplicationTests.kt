5dd923e0e61fc44e7b5761eff5f669b637c95d13
Julian Ritondale <jritondale@mail.austral.edu.ar>
2025-11-26 15:41:08 -0300

    feat/fix: add language and status objects, cleanup

---
+++ src/test/kotlin/com/example/snippetsearcher/ExecutionServiceApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.example.snippetsearcher
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ExecutionServiceApplicationTests {
+
+    @SuppressWarnings("EmptyFunctionBlock")
+    @Test
+    fun contextLoads() {}
+}
